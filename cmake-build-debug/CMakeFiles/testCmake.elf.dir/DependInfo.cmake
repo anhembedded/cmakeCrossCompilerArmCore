ca0dc377d76b1f7843e277290ba5ba0a6fd814c4
Anh.Embedded <Anh.Embedded@gmail.com>
2022-10-19 16:00:26 +0700

    the frist commit

---
+++ cmake-build-debug/CMakeFiles/testCmake.elf.dir/DependInfo.cmake
@@ -0,0 +1,80 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Startup/startup_stm32f407vgtx.s" "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/cmake-build-debug/CMakeFiles/testCmake.elf.dir/Core/Startup/startup_stm32f407vgtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../USB_HOST/App"
+  "../USB_HOST/Target"
+  "../Core/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Middlewares/ST/STM32_USB_Host_Library/Core/Inc"
+  "../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/main.c" "CMakeFiles/testCmake.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/stm32f4xx_hal_msp.c" "CMakeFiles/testCmake.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/stm32f4xx_it.c" "CMakeFiles/testCmake.elf.dir/Core/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/stm32f4xx_it.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/syscalls.c" "CMakeFiles/testCmake.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/sysmem.c" "CMakeFiles/testCmake.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Core/Src/system_stm32f4xx.c" "CMakeFiles/testCmake.elf.dir/Core/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Core/Src/system_stm32f4xx.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_hcd.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2s_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_spi.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_usb.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src/usbh_cdc.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_core.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ctlreq.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_ioreq.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/Middlewares/ST/STM32_USB_Host_Library/Core/Src/usbh_pipes.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/USB_HOST/App/usb_host.c" "CMakeFiles/testCmake.elf.dir/USB_HOST/App/usb_host.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/USB_HOST/App/usb_host.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/USB_HOST/Target/usbh_conf.c" "CMakeFiles/testCmake.elf.dir/USB_HOST/Target/usbh_conf.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/USB_HOST/Target/usbh_conf.c.obj.d"
+  "C:/Users/hoang/STM32CubeIDE/workspace_1.10.1/testCmake/USB_HOST/Target/usbh_platform.c" "CMakeFiles/testCmake.elf.dir/USB_HOST/Target/usbh_platform.c.obj" "gcc" "CMakeFiles/testCmake.elf.dir/USB_HOST/Target/usbh_platform.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
